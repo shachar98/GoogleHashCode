33ec09bf634be11f993d5936e19248f559cb1636
shachar <shachar-har-zvi@hotmail.com>
2019-01-23 21:11:13 +0200

    FDS

---
+++ PizzaProblem/ProblemInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaProblem
+{
+    public class ProblemInput
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public int MinIngredients { get; set; }
+        public int MaxSliceSize { get; set; }
+
+        public Cell[,] Cells { get; set; }
+    }
+
+    public class Cell
+    {
+    }
+}
---
+++ PizzaProblem/Solver.cs
@@ -0,0 +1,17 @@
+﻿using HashCodeCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaProblem
+{
+    public class Solver : SolverBase<ProblemInput, ProblemOutput>
+    {
+        protected override ProblemOutput Solve(ProblemInput input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
