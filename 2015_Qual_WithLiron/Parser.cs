5d48e2af9eb6b8bb1ddf6ad9ce7d24d2d1a10087
shachar <shachar-har-zvi@hotmail.com>
2019-02-23 19:50:05 +0200

    fas

---
+++ 2015_Qual_WithLiron/Parser.cs
@@ -0,0 +1,18 @@
+﻿using HashCodeCommon;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015_Qual_WithLiron
+{
+    public class Parser : ParserBase<ProblemInput>
+    {
+        protected override ProblemInput ParseFromStream(TextReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 2015_Qual_WithLiron/ScoreCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015_Qual_WithLiron
+{
+    class ScoreCalculator
+    {
+    }
+}
