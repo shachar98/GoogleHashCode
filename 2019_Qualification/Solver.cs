98e0d221a885ab13c9e6baa275bf34c1179808f3
shachar <shachar-har-zvi@hotmail.com>
2019-02-28 20:29:09 +0200

    fsd

--- 2019_Qualification/Solver.cs
+++ 2019_Qualification/Solver.cs
@@ -11,8 +11,29 @@ namespace _2019_Qualification
     {
         protected override ProblemOutput Solve(ProblemInput input)
         {
+            // Sort the input for preformance
+            // Split the input for preformance
+
+
+            foreach (var curr in input.Photos)
+            {
+                foreach(var curr2 in input.Photos)
+                {
+                    var score = CalcScore(curr,curr2);
+                }
+            }
+
             // Solve the problem
             throw new NotImplementedException();
+        }
+
+        private int CalcScore(Photo first, Photo second)
+        {
+            int together = first.Tags.Count(_ => second.Tags.Any(__ => __ == _));
+            int onlySecond = second.Tags.Count - together;
+            int onlyFirst = first.Tags.Count - together;
+
+            return Math.Min(together, Math.Min(onlyFirst, onlySecond));
         }
     }
 }
