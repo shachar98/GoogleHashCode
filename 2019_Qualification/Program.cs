4625cc6a22d518a33d6197ee0eb8d6d80413a5a7
shachar <shachar-har-zvi@hotmail.com>
2019-03-01 14:22:47 +0200

    Add everything

--- 2019_Qualification/Program.cs
+++ 2019_Qualification/Program.cs
@@ -13,30 +13,30 @@ namespace _2019_Qualification
         {
             Calculator calculator = new Calculator();
 
-            //Runner<ProblemInput, ProblemOutput> runner2 = new Runner<ProblemInput, ProblemOutput>(
-            //    "2019", new Parser(), new Solver(), new Printer(), calculator);
-            //runner2.Run(Properties.Resources.a, "2019_a", 1, true);
+            Runner<ProblemInput, ProblemOutput> runner2 = new Runner<ProblemInput, ProblemOutput>(
+                "2019", new Parser(), new Solver(), new Printer(), calculator);
+            runner2.Run(Properties.Resources.a, "2019_a", 1, true);
 
             Console.WriteLine(DateTime.Now);
 
-            //Runner<ProblemInput, ProblemOutput> runner3 = new Runner<ProblemInput, ProblemOutput>(
-            //   "2019", new Parser(), new Solver(), new Printer(), calculator);
-            //runner3.Run(Properties.Resources.b, "2019_b", 1, true);
-            //Console.WriteLine(DateTime.Now);
+            Runner<ProblemInput, ProblemOutput> runner3 = new Runner<ProblemInput, ProblemOutput>(
+               "2019", new Parser(), new Solver(), new Printer(), calculator);
+            runner3.Run(Properties.Resources.b, "2019_b", 1, true);
+            Console.WriteLine(DateTime.Now);
 
-            //Runner<ProblemInput, ProblemOutput> runner4 = new Runner<ProblemInput, ProblemOutput>(
-            //   "2019", new Parser(), new Solver(), new Printer(), calculator);
-            //runner4.Run(Properties.Resources.c, "2019_c", 1, true);
-            //Console.WriteLine(DateTime.Now);
+            Runner<ProblemInput, ProblemOutput> runner4 = new Runner<ProblemInput, ProblemOutput>(
+               "2019", new Parser(), new Solver(), new Printer(), calculator);
+            runner4.Run(Properties.Resources.c, "2019_c", 1, true);
+            Console.WriteLine(DateTime.Now);
 
             Runner<ProblemInput, ProblemOutput> runner5 = new Runner<ProblemInput, ProblemOutput>(
                "2019", new Parser(), new Solver(), new Printer(), calculator);
             runner5.Run(Properties.Resources.d, "2019_d", 1, true);
             Console.WriteLine(DateTime.Now);
 
-            //Runner<ProblemInput, ProblemOutput> runner6 = new Runner<ProblemInput, ProblemOutput>(
-            //   "2019", new Parser(), new Solver(), new Printer(), calculator);
-            //runner6.Run(Properties.Resources.e, "2019_e", 1, true);
+            Runner<ProblemInput, ProblemOutput> runner6 = new Runner<ProblemInput, ProblemOutput>(
+               "2019", new Parser(), new Solver(), new Printer(), calculator);
+            runner6.Run(Properties.Resources.e, "2019_e", 1, true);
 
             Console.WriteLine(DateTime.Now);
 
